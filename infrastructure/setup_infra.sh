2f8264ac70fa74d72a6d0143c96afee9a3b9f082
matiaspetre <48700482+matiaspetre@users.noreply.github.com>
2019-03-19 10:18:31 -0300

    update setup_infra.sh

--- infrastructure/setup_infra.sh
+++ infrastructure/setup_infra.sh
@@ -16,10 +16,10 @@ sudo yum update -y
 # setup some variables
 
 echo "\
-export YOUR_GITHUB_USER='thywoof'
+export YOUR_GITHUB_USER='matiaspetre'
 
-export YOUR_NAME='Winston Wolfe'
-export YOUR_COMPANY_NAME='Marsellus Wallace Inc.'
+export YOUR_NAME='Matias Petre'
+export YOUR_COMPANY_NAME='NewRelic Workshoop'
 
 export YOUR_LICENSE_KEY=cec283018fe0214f68b46ecc1223ffc43818d5ca
 
